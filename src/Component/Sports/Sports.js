eb5b9a7ed49f4651404209b05f5fbdfe8daabefd
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-11-02 22:02:32 +0600

    second-commit

--- src/Component/Sports/Sports.js
+++ src/Component/Sports/Sports.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee,faLocation } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Sports.css'
@@ -9,8 +11,9 @@ const Sports = () => {
        .then(data=>setCart(data));
     },[])
     return (
-        <div>
-          <h1 style={{marginLeft:"20px",marginBottom:"-7px"}}>Select today’s exercise</h1> 
+        <div className='container'>
+            <div>
+            <h1 style={{marginLeft:"20px",marginBottom:"-7px"}}>Select today’s exercise</h1> 
           <div className='sports-container'>
            {
             cart.map(product=><Product 
@@ -18,6 +21,50 @@ const Sports = () => {
                 product={product}
                 ></Product>)
            }
+          </div>
+            </div>
+      
+          <div className='cart-container'>
+            <div>
+            <h4>Khalid Emon</h4>
+             <div className='second'>
+             <FontAwesomeIcon icon={faLocation}></FontAwesomeIcon>
+             <p style={{marginLeft:'15px',marginTop:'-3px'}}>Dhaka,Bangladesh</p>
+            </div>     
+            <div>
+          
+             <div className='third'>
+             <div className='container-1'>
+              <h1>55kg</h1>
+              <h1>5.5</h1>
+              <h1>25Yrs</h1>
+            </div>
+            <div className='container-2'>
+              <p>Weight</p>
+              <p>height</p>
+              <p>Age</p>
+            </div>
+            </div>           
+             </div>
+             </div> 
+
+            <h3>Add a Break</h3>
+            <div className='container-3'>
+          <h6 className='container-4'>10s</h6>
+          <h6 className='container-5'>20s</h6>
+          <h6 className='container-6'>30s</h6>
+          <h6 className='container-7'>40s</h6>
+          <h6 className='container-8'>50s</h6>
+         </div>
+
+         <h2 style={{marginTop:'40px'}}>Exercise Details</h2>
+
+         <div className='container-9'>
+            <label>Exercise time : </label>
+            <input type={"text"}></input>
+          </div>
+
+
           </div>
         </div>
     );
