7cca6a81ab6b57f95218af795ecb7344b2cbe506
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-11-05 22:33:12 +0600

    fifth-commit

--- src/Component/calculate/calculate.js
+++ src/Component/calculate/calculate.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './calculate.css'
 const calculate = ({cart}) => {
-    console.log(cart);
     return (
         <div className='container-12'>
               {cart.length}
