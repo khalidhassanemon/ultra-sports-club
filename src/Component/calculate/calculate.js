98d52a7f4acf0fb7d5b3dff96e229d3d13af48e6
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-11-06 00:28:59 +0600

    last-commit-list

--- src/Component/calculate/calculate.js
+++ src/Component/calculate/calculate.js
@@ -7,10 +7,10 @@ const calculate = ({cart}) => {
       const i=parseInt(product.Time);
         totalTime=totalTime+i;
     }
-
+    if(totalTime>0)
     return (
         <div className='container-12'>
-             <div>: {totalTime}s</div> 
+             <div>{totalTime}s</div> 
         </div>
     );
 };
