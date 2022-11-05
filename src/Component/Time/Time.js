69fc23bb83957553ae6d2f61d349bb6813aa05a1
khalidhassanemon <khalidhassanemon12@gmail.com>
2022-11-06 00:26:39 +0600

    last-but-not-list

--- src/Component/Time/Time.js
+++ src/Component/Time/Time.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Time.css';
 const Time = (props) => {
+    if(props.num>0)
     return (
         <div className='time'>
-            <p>{props.num}s</p>
+          
+                <p1>{props.num}s</p1>
+                     
         </div>
     );
 };
